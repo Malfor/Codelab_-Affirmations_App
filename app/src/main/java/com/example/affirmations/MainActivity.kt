a3a50b47590de2ccec5047806b296c987497c3f2
Rodolfo Barrios <rodolfo.barrios@avista.co>
2021-02-22 14:15:34 -0500

    Se configura el adapter y se muestra en pantalla

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        configAdapter()
+    }
+
+    private fun configAdapter() {
+        binding.recyclerView.adapter = ItemAdapter(this, Datasource().loadAffirmations())
+        binding.recyclerView.setHasFixedSize(true)
     }
 }
